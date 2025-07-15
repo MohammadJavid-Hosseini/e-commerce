5004dc92bca32bd1156b360e0832fc23298bb3da
MohammadJavid-Hosseini <javidbusinesstech@gmail.com>
2025-07-15 15:57:02 +0330

    Add serializer, view, and URL for user registration endpoint.

--- account/views.py
+++ account/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import AllowAny
+from account.serializers import UserSerializer
 
-# Create your views here.
+User = get_user_model()
+
+
+class RegistrationAPIView(CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
+
