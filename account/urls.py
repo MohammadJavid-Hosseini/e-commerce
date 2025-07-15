a2765a8d70751501782b950297922bfa84e865a1
MohammadJavid-Hosseini <javidbusinesstech@gmail.com>
2025-07-15 17:21:27 +0330

    Add login endpoint using JWT to pass login test.

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView
 from account import views
 
 
 urlpatterns = [
-    path('register/', views.RegistrationAPIView.as_view(), name='register')
+    path('register/', views.RegistrationAPIView.as_view(), name='register'),
+    path('login/', TokenObtainPairView.as_view(), name='login')
 ]
