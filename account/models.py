2c2e9a5806e9e58c941a5571b8cbe0a62cf4012b
MohammadJavid-Hosseini <javidbusinesstech@gmail.com>
2025-07-15 13:34:03 +0330

    Models Address and User created in account app.

--- account/models.py
+++ account/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Address(models.Model):
+    label = models.CharField(max_length=255)
+    address_line_1 = models.TextField(max_length=500)
+    address_line_2 = models.TextField(max_length=500)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=255)
+    postal_code = models.CharField(max_length=11)
+    country = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.label}, {self.address_line_1[:30]}"
+
+
+class User(AbstractUser):
+    phone = models.CharField(max_length=16)
+    address = models.ForeignKey(to=Address, on_delete=models.SET_NULL, null=True, blank=True)
+    is_seller = models.BooleanField(default=False)
+    picture = models.ImageField(upload_to="users/", null=True, blank=True)
+
+    def __str__(self):
+        return self.username
+
--- config/settings.py
+++ config/settings.py
@@ -128,3 +128,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Set the costumized user as user auth model
+AUTH_USER_MODEL = 'account.User'
