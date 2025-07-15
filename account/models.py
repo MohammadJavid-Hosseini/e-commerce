6c3174900e830eaf39c624f69ba6f711583fe0df
MohammadJavid-Hosseini <javidbusinesstech@gmail.com>
2025-07-15 14:09:52 +0330

    Add database indexes to improve query performance.

--- account/models.py
+++ account/models.py
@@ -6,23 +6,27 @@ class Address(models.Model):
     label = models.CharField(max_length=255)
     address_line_1 = models.TextField(max_length=500)
     address_line_2 = models.TextField(max_length=500)
-    city = models.CharField(max_length=255)
-    state = models.CharField(max_length=255)
-    postal_code = models.CharField(max_length=11)
-    country = models.CharField(max_length=255)
+    city = models.CharField(max_length=255, db_index=True)
+    state = models.CharField(max_length=255, db_index=True)
+    postal_code = models.CharField(max_length=11, db_index=True)
+    country = models.CharField(max_length=255, db_index=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.label}, {self.address_line_1[:30]}"
 
+    class Meta:
+        indexex = [
+            models.index(fields=['city', 'state', 'country'])
+        ]
+
 
 class User(AbstractUser):
-    phone = models.CharField(max_length=16)
+    phone = models.CharField(max_length=16, db_index=True)
     address = models.ForeignKey(to=Address, on_delete=models.SET_NULL, null=True, blank=True)
-    is_seller = models.BooleanField(default=False)
+    is_seller = models.BooleanField(default=False, db_index=True)
     picture = models.ImageField(upload_to="users/", null=True, blank=True)
 
     def __str__(self):
         return self.username
-
