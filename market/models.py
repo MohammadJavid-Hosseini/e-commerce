2c134d7f5ff7e133f54cd7c7b3dac3b33f2bc18e
MohammadJavid-Hosseini <javidbusinesstech@gmail.com>
2025-08-13 17:30:47 +0330

    fix(model): add 'status' and 'explanation' fields to OrderItem and Order respectively

--- market/models.py
+++ market/models.py
@@ -26,12 +26,24 @@ RATING_CHOICES = [
     (RATING_3, 'Good'), (RATING_2, 'Not good'), (RATING_1, 'Awful')
 ]
 
+# OrderItem status
+ORDERITEM_STATUS_PENDING = 'pending'
+ORDERITEM_STATUS_REJECTED = 'rejected'
+ORDERITEM_STATUS_CONFIRMED = 'confirmed'
+
+ORDERITEM_CHOICES = [
+    (ORDERITEM_STATUS_PENDING, 'Pending'),
+    (ORDERITEM_STATUS_REJECTED, 'Rejected'),
+    (ORDERITEM_STATUS_CONFIRMED, 'confirmed')
+]
+
 # Order status
 ORDER_STATUS_PENDING = 'pending'
 ORDER_STATUS_PROCESSING = 'processing'
 ORDER_STATUS_DELIVERED = 'delivered'
 ORDER_STATUS_CANCELLED = 'cancelled'
 ORDER_STATUS_FAILED = 'failed'
+# OPTIMIZE: add a 'shipped' status as well
 
 STATUS_CHOICES = [
     (ORDER_STATUS_PENDING, 'Pending'),
@@ -195,6 +207,7 @@ class Order(TimeStampedModel, SoftDeleteModel):
         max_length=12, choices=STATUS_CHOICES, default=ORDER_STATUS_PENDING)
     total_price = models.DecimalField(
         max_digits=10, decimal_places=2, default=0)
+    explanation = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return f"{self.customer.username} - order {self.id}"
@@ -222,6 +235,8 @@ class OrderItem(TimeStampedModel, SoftDeleteModel):
         max_digits=10, decimal_places=2, default=0)
     final_price = models.DecimalField(
         max_digits=10, decimal_places=2, default=0)
+    status = models.CharField(
+        max_length=12, choices=ORDERITEM_CHOICES, default=ORDER_STATUS_PENDING)
 
     def __str__(self):
         return f"{self.order} - {self.store_item.name}"
