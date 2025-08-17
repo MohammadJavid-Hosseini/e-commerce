f2b09be6b5863ddacd73cd100fab172bab7bab69
MohammadJavid-Hosseini <javidbusinesstech@gmail.com>
2025-08-17 14:52:19 +0330

    fix(serializer): modify update method in CartItemSerializer to return the instance

--- market/serializers.py
+++ market/serializers.py
@@ -184,8 +184,11 @@ class CartItemSerializer(serializers.ModelSerializer,
         quantity = validated_data.get('quantity')
         if quantity <= 0:
             raise serializers.ValidationError("Quantity must not be 0")
+        instance.quantity = quantity
+        instance.save()
 
-    # NOTE: when creating a cart item, if quantity is not passed set it 1
+        return instance
+    # OPTIMIZE: when creating a cart item, if quantity is not passed set it 1
 
 
 class MiniCartItemSerializer (serializers.ModelSerializer,
